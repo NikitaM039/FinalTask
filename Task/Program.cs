e78b62d02a5334fb935082fbc623bd10c7f44a3b
Nikita Malyshev Aorus <nikita110097@mail.ru>
2023-03-25 22:46:02 +0200

    Add method for selection

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,3 +3,16 @@ Console.WriteLine("Введите массив строк через пробе�
 string? row = Console.ReadLine();
 array = row.Split(' ');
 string[] array2 = new string[array.Length];
+
+void SecondArrayWithIF(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
